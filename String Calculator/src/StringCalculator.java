0359e7c0b82131f9ab39c7d41a94f9b595bc0a8c
SanjayElanGCIT <sanjay.elavazhagan@gcitsolutions.com>
2023-05-25 15:44:34 +0530

    handling empty string in String calculator

--- String Calculator/src/StringCalculator.java
+++ String Calculator/src/StringCalculator.java
@@ -3,19 +3,33 @@ import java.io.*;
 public class StringCalculator {
 
     public static int Add ( String numbers ) {
+
         String[] digits = numbers.split(",");
+
         int result = 0;
-        int[] a = new int[2];
+        int a[] = new int[2];
+
+        for (int j=0; j<digits.length; j++) {
+            if (digits[j].isEmpty()) {
+                digits[j] = "0";
+            }
+        }
+
         a[0] = Integer.parseInt(digits[0]);
         a[1] = Integer.parseInt(digits[1]);
         result = a[0] + a[1];
+
         System.out.println("The Sum of two value is: "+result);
         return 0;
+
     }
     public static void main(String args[]) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Enter a two valid number string separated bt comma(,)");
         String value = br.readLine();
+
         Add(value);
     }
 }
