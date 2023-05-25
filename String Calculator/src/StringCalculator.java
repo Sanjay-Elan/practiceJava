25ea077b08c6b6152b6e3deccc24374276956c67
SanjayElanGCIT <sanjay.elavazhagan@gcitsolutions.com>
2023-05-25 16:38:00 +0530

    handling negative arguments in the String calculator input

--- String Calculator/src/StringCalculator.java
+++ String Calculator/src/StringCalculator.java
@@ -10,14 +10,28 @@ public class StringCalculator {
 
         int result = 0;
 
-        for ( int j = 0; j < digits.length; j++ ) {
-            if ( digits[j].isEmpty() ) {
+        for (int j=0; j<digits.length; j++) {
+
+            if (digits[j].isEmpty()) {
+
                 digits[j] = "0";
+
             }
+
         }
 
         for ( int i = 0; i < digits.length; i++ ) {
-            result += Integer.parseInt(digits[i]);
+
+            if ( Integer.parseInt(digits[i]) < 0 ) {
+
+                throw new IllegalArgumentException();
+
+            }
+            else {
+
+                result += Integer.parseInt(digits[i]);
+
+            }
         }
 
         System.out.println("The Sum of two value is: "+result);
