864031fc8205d39563c426ddbf6b24384423e65f
SanjayElanGCIT <sanjay.elavazhagan@gcitsolutions.com>
2023-06-02 15:09:53 +0530

    Adding synchronus and Asynchronus multithreading practice codes

---
+++ Multithreading/Synchronised Multithreading/src/Main.java
@@ -0,0 +1,39 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+class Printer extends Thread{
+    synchronized static void print ( String args[]) {
+        Thread thread;
+        try {
+            for ( int i =0; i< args.length; i++ ){
+                System.out.println(args[i]);
+                sleep(300);
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+}
+class Thread1 extends Thread {
+    String a[] = {"Orange","Apple","Banana"};
+    public void run () {
+        Printer.print(a);
+    }
+}
+class Thread2 extends Thread {
+    String b[] = {"Potato","Cabbage","Tomato"};
+    public void run () {
+        Printer.print(b);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start();
+        t2.start();
+    }
+}
